56ce4e3b5d89002214b897358e2c48e7a1bd76de
Austin Nickel <austinnickel@Austins-MacBook-Pro.local>
2024-04-25 18:30:38 -0700

    Added a hardcode array to SearchResults component and added a useState hook for SearchResults

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -1,7 +1,32 @@
+import {useState} from 'react'
+
+const results = [
+    {
+        name: "Lemonade",
+        artist: "Beyonce",
+        album: "Lemonade",
+        id: "Lemonade-Beyonce"
+    },
+
+    {
+        name: "Ain't No Thief",
+        artist: "Viagra Boys",
+        album: "Cave World",
+        id: "Ain'tNoThief-ViagraBoys"
+    },
+
+    {
+        name: "For Whom the Bell Tolls",
+        artist: "Metallica",
+        album: "Ride the Lightning",
+        id: "ForWhomtheBellTolls-Metallica"
+    }
+]
 
 
-function SearchResults() {
 
+function SearchResults(result) {
+const [result, setResult] = useState(null);
 }
 
 export default SearchResults;
