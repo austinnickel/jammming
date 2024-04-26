9b60224413b018985d4099a48fcbba413d64098d
Austin Nickel <austinnickel@Austins-MacBook-Pro.local>
2024-04-25 19:29:52 -0700

    Hardcoded a test playlist and set up props to be passed to my Playlist and Tracklist components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
-import SearchResults from './components/SearchResults/SearchResults'
+import SearchResults from './components/SearchResults/SearchResults';
+import Playlist from './components/Playlist/Playlist';
+import Tracklist from './components/Tracklist/Tracklist';
 import './App.css'
 
 function App() {
@@ -9,26 +11,53 @@ function App() {
         name: "Lemonade",
         artist: "Beyonce",
         album: "Lemonade",
-        id: "Lemonade-Beyonce"
+        id: 1
     },
 
     {
         name: "Ain't No Thief",
         artist: "Viagra Boys",
         album: "Cave World",
-        id: "Ain'tNoThief-ViagraBoys"
+        id: 2
     },
 
     {
         name: "For Whom the Bell Tolls",
         artist: "Metallica",
         album: "Ride the Lightning",
-        id: "ForWhomtheBellTolls-Metallica"
+        id: 3
     }
-]
+];
+
+const mockPlaylist = {
+  name: "My New Playlist", 
+  tracks: [
+    {
+      id: 4,
+      name: "Damnit",
+      artist: "Blink-182",
+      album: "Dude Ranch"
+    },
+    {
+      id: 5,
+      name: "Stairway to Heaven",
+      artist: "Led Zeppelin",
+      album: "Zozo"
+    },
+    {
+      id: 6,
+      name: "When the Doves Cry",
+      artist: "Prince",
+      album: "Purple Rain"
+    }
+
+  ]
+};
 
   return (
     <>
+    <Playlist name={mockPlaylist.name} />
+    <Tracklist tracks={mockPlaylist.tracks} />
     <SearchResults initialResults={initialResults} />  
     </>
   )
