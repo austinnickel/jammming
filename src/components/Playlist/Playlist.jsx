b35c89bdb830a49d42a70a7935a22c416f459888
Austin Nickel <austinnickel@Austins-MacBook-Pro.local>
2024-04-25 20:02:24 -0700

    Made the local states and added the jsx to render both my Tracklist and Playlist components

--- src/components/Playlist/Playlist.jsx
+++ src/components/Playlist/Playlist.jsx
@@ -1,7 +1,13 @@
+import {useState} from 'react';
 
+function Playlist({name}) {
+const [playListName, setPlayListName] =useState(name);
 
-function Playlist() {
-
+return (
+    <div>
+        <h2>Playlist Name: {name}</h2>
+    </div>
+)
 }
 
 export default Playlist;
